437958a92bc82ae416216439d61a70b6fdf725c4
Jesly_Joseph <jesly.joseph@tarento.com>
2023-08-25 15:07:17 +0530

    dashborad for admin

---
+++ upsmf-registration-and-ecreds-web-module/src/app/modules/super-admin/super-admin.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SuperAdminRoutingModule } from './super-admin-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from '../shared';
+import { MaterialModule } from 'src/app/material/material.module';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    SuperAdminRoutingModule,
+    SharedModule,
+    MaterialModule
+  ]
+})
+export class SuperAdminModule { }
